dc1c2966074c27a00776e83a6c56776af3e5804e
Annie Hosch <anniehosch@Annies-Air.lan>
2022-07-07 12:28:55 -0400

    save city function is javascript, updated readme w user story and acceptance crit

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-var searchInputEl = document.getElementById("search-input")
-var formatInputEl = document.getElementById("format-input")
-var searchBtnEl = document.getElementById("search-btn")
+var keyAPI = "1ced721104d07f711043eeabff75388a";
+var searchInputEl = document.getElementById("search-input");
+var searchBtnEl = document.getElementById("search-btn");
+var searchCity = "";
 
-searchBtnEl.addEventListener("click", getParams)
+searchBtnEl.addEventListener("click", saveCity)
 
-function getParams (event) {
+function saveCity (event) {
     event.preventDefault()
     var searchTerm = searchInputEl.value
-    var format = formatInputEl.value
     if (searchTerm === ""){
         alert("Enter a city!")
         return
     }
-    apiSearch(searchTerm, format)
+    else (saveCity(city));
+    apiSearch(searchTerm, city)
 }
 
-function 
+function apiSearch(search, city){
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + keyAPI;
+    fetch(requestUrl)
+    .then(function (response){
+        if (!response.ok){
+            console.log("error getting data")
+        } else {
+            return response.json()
+        }
+
+    })
+    .then(response (display){
+        saveCity(city);
+        let currentCityWeatherHTML = 
+
+        $('#current-city-weather').html(currentCityWeatherHTML)
+
+    })
+}
+function printData(data){
+
+}
