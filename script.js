fbf2afc1006af178f0bd4d943aad08454ec3dd86
Annie Hosch <anniehosch@Annies-Air.lan>
2022-07-08 00:07:48 -0400

    added global vars, tutor help

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ var keyAPI = "1ced721104d07f711043eeabff75388a";
 var searchInputEl = document.getElementById("search-input");
 var searchBtnEl = document.getElementById("search-btn");
 var searchCity = "";
+var searchTerm
+
 
 searchBtnEl.addEventListener("click", saveCity)
 
 function saveCity (event) {
     event.preventDefault()
-    var searchTerm = searchInputEl.value
-    if (searchTerm === ""){
-        alert("Enter a city!")
-        return
-    }
-    else (saveCity(city));
-    apiSearch(searchTerm, city)
+    searchTerm = event.target.value
+        console.log(event.target.value)
+        
+    // if (searchTerm === ""){
+    //     alert("Enter a city!")
+    //     return
+    // }
+    // else 
+    // (saveCity(city));
+    // apiSearch(searchTerm, city)
+    // input.addEventListener
+    // const input = document.querySelector("#search-input")
+    // input.addEventListener("click", function()
+    //     console.log("input vlaue", input.value)
+    //     cities.push(input.value)
+    //     localStorage.setItem("cities", cities))
 }
+
+// let cities = []
+
 
 function apiSearch(search, city){
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + keyAPI;
@@ -27,14 +41,16 @@ function apiSearch(search, city){
         }
 
     })
-    .then(response (display){
-        saveCity(city);
-        let currentCityWeatherHTML = 
+    .then(function (data){
+        console.log(data)
+        // saveCity(city);
+        // let currentCityWeatherHTML = data;
 
-        $('#current-city-weather').html(currentCityWeatherHTML)
+        // ('#current-city-weather').html(currentCityWeatherHTML)
 
     })
 }
-function printData(data){
+// function printData(data){
 
-}
+// function
+// }
